15b573c64c06ce1b0de96dffa570290418988bf5
Alpha App <philipmanx@gmail.com>
2018-08-29 15:30:51 +0000

    add admin tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,7 +5,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @category = Category.create(name: "sports")
-    
+    @user = User.create(username: "john", email: "john@example.com", password: "welcome123", admin: true)
   end
   
   test "should get categories index" do
@@ -16,6 +16,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get new" do
+    sign_in_as(@user, "welcome123")
     get new_category_path
     assert_response :success
     
@@ -27,6 +28,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     
     
+  end
+  
+  test "should redirect create category when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path, "able to post to action did not get redirected"
   end
   
 end
